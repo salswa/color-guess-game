6dadbdc513fe6fec3c2f81b39ac49b97feb9096c
salswa <swatindp@gmail.com>
2025-05-07 23:40:11 +0530

    script.js file code improvement

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ const colorCodeContainer = document.querySelector(".color-code");
 const colorOptionsContainer = document.querySelector(".color-options");
 const scoreValueContainer = document.querySelector(".score .value");
 
-/** Random Number Generator Method */
+/**
+ * LocalStorage Keys
+ */
+const CURRENTSCORE = "currentScore";
+const QUESTIONCOLOR = "questionColor";
+const COLOROPTIONS = "colorOptions";
 
+/**
+ * Random Number Generator Method
+ * @param {*} min
+ * @param {*} max
+ * @returns random number between min and max (inclusive)
+ */
 function generateRandomNumberBetween(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-/** Random Color Generator */
-
+/**
+ * Random Color Generator
+ * @returns a color in rgb format
+ */
 function generateRandomRGBColor() {
   const red = generateRandomNumberBetween(0, 255);
   const green = generateRandomNumberBetween(0, 255);
@@ -17,15 +30,44 @@ function generateRandomRGBColor() {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-/** Question Color Generator */
+/**
+ * Localstorage Setters and Getters
+ */
+function setScore(score) {
+  localStorage.setItem(CURRENTSCORE, score);
+}
 
+function getScore() {
+  return Number(localStorage.getItem(CURRENTSCORE));
+}
+
+function setQuestionColor(color) {
+  localStorage.setItem(QUESTIONCOLOR, color);
+}
+
+function setColorOptions(colors) {
+  localStorage.setItem(COLOROPTIONS, JSON.stringify(colors));
+}
+
+function getQuestionColor() {
+  return localStorage.getItem(QUESTIONCOLOR);
+}
+
+function getColorOptions() {
+  return JSON.parse(localStorage.getItem(COLOROPTIONS));
+}
+
+/**
+ * Question Color Generator
+ */
 function generateQuestionColor() {
   const color = generateRandomRGBColor();
   setQuestionColor(color);
 }
 
-/** Mulitple Color Options Generator  */
-
+/**
+ * Mulitple Color Options Generator
+ */
 function generateColorOptions() {
   const colorOptions = [];
   const ansIndex = generateRandomNumberBetween(0, 5);
@@ -39,8 +81,9 @@ function generateColorOptions() {
   setColorOptions(colorOptions);
 }
 
-/** START | Methods to display related values on HTML */
-
+/**
+ * Methods to display related values on HTML
+ */
 function displayQuestionColor() {
   colorCodeContainer.innerHTML = getQuestionColor();
 }
@@ -60,36 +103,10 @@ function displayColorOptionsBox() {
     colorOptionsContainer.appendChild(box);
   }
 }
-/** END | Methods to display related values on HTML */
 
-/** START | localstorage Setters and Getters */
-
-function setScore(score) {
-  localStorage.setItem("currentScore", score);
-}
-
-function getScore() {
-  return Number(localStorage.getItem("currentScore"));
-}
-
-function setQuestionColor(color) {
-  localStorage.setItem("questionColor", color);
-}
-
-function setColorOptions(colors) {
-  localStorage.setItem("colorOptions", JSON.stringify(colors));
-}
-
-function getQuestionColor() {
-  return localStorage.getItem("questionColor");
-}
-
-function getColorOptions() {
-  return JSON.parse(localStorage.getItem("colorOptions"));
-}
-
-/** END | localstorage Setters and Getters */
-
+/**
+ * Function to validate selected answer
+ */
 function validateSelection(e) {
   const selectedColor = e.target.style.backgroundColor;
   console.log(selectedColor);
@@ -106,29 +123,41 @@ function validateSelection(e) {
   displayScore();
 }
 
+/**
+ * Generate new colors wrapper function
+ */
 function generateNewColors() {
   generateQuestionColor();
   generateColorOptions();
 }
 
+/**
+ * Display colors wrapper function
+ */
 function displayColors() {
   colorOptionsContainer.innerHTML = "";
   displayQuestionColor();
   displayColorOptionsBox();
 }
 
+/**
+ * Reset colors
+ */
 function resetColors() {
   generateNewColors();
   displayColors();
 }
 
+/**
+ * Initialize Game
+ */
 function initGame() {
-  if (!localStorage.hasOwnProperty("questionColor")) {
+  if (!localStorage.hasOwnProperty(QUESTIONCOLOR)) {
     generateNewColors();
   }
   displayColors();
 
-  if (localStorage.hasOwnProperty("currentScore")) {
+  if (localStorage.hasOwnProperty(CURRENTSCORE)) {
     displayScore();
   } else {
     setScore(0);
